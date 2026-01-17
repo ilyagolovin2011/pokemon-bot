3cccea44f238fd0367c8aa58f5c96d6d976e1e26
ilyagolovin2011 <ilyagolovin2011@gmail.com>
2026-01-17 17:15:31 +0500

    Бот готов

---
+++ logic.py
@@ -0,0 +1,56 @@
+from random import randint
+import requests
+
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+        self.number = self.get_number()
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+    # Метод для получения картинки покемона через API
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['official-artwork']["front_shiny"])
+        else:
+            return "https://img.freepik.com/premium-photo/girl-pikachu_551707-69798.jpg?semt=ais_hybrid&w=740"
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+
+    def get_number(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['id'])
+        else:
+            return "Pikachu"
+
+    # Метод класса для получения информации
+    def info(self):
+        return f"Имя твоего покемона: {self.name}, Номер твоего покемона: {self.number}"
+
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+
+
+
