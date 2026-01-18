c6de1977ff48d8289a4ccd5c6954eeed2753224f
ilyagolovin2011 <ilyagolovin2011@gmail.com>
2026-01-18 19:25:37 +0500

    Добавлена атака

--- logic.py
+++ logic.py
@@ -16,6 +16,9 @@ class Pokemon:
         self.height = self.get_height()
         self.type = self.get_type()
 
+        self.hp = randint(100, 200)
+        self.power = randint(10, 20)
+
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
@@ -73,8 +76,18 @@ class Pokemon:
             return (data['types'][0]['type']['name'])
         else:
             return "Pikachu"
-        
-    
+
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
 
     # Метод класса для получения информации
     def info(self):
@@ -83,8 +96,22 @@ class Pokemon:
     Вес твоего покемона: {self.weight}
     Высота твоего покемона: {self.height}
     Тип твоего покемона: {self.type}
+    Сила твоего покемона: {self.power}
+    Здоровье твоего покемона: {self.hp}
+    
 """
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+    
+class Wizard(Pokemon):
+    pass
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
